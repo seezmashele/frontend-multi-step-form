e95b4a492c93898266c13d7c988fbcf18fa6f9eb
seezmashele <seez.mash@hotmail.com>
2023-07-29 14:45:26 +0200

    step 1 form validation finished

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom"
-
 const Sidebar = ({ selectedIndex = 0 }) => {
   const steps = [
     { title: "Your info", path: "/" },
@@ -13,21 +11,22 @@ const Sidebar = ({ selectedIndex = 0 }) => {
       <div className="px-6 pb-28 pt-10 md:pb-8 my:pt-8 gap-y-3 gap-x-2 md:gap-x-2 justify-center md:justify-normal flex md:flex-col">
         {steps.map((item, index) => {
           return (
-            <Link to={item.path} key={`sidebar-item${index}`}>
-              <div className="flex items-center hover:bg-[#ffffff22] cursor-pointer py-2 px-2 box_radius select-none">
-                <div
-                  className={`w-10 h-10 md:w-8 md:h-8 rounded-full flex-shrink-0 md:mr-4 border border-white flex items-center justify-center font-bold ${
-                    selectedIndex === index && "milky_cyan_bg text-black"
-                  }`}
-                >
-                  {index + 1}
-                </div>
-                <div className="uppercase hidden md:block">
-                  <h4 className="text-xs opacity-60">Step {index + 1}</h4>
-                  <h4 className="font-bold tracking-wider">{item.title}</h4>
-                </div>
+            <div
+              key={`sidebar-item${index}`}
+              className="flex items-center py-2 px-2 box_radius select-none"
+            >
+              <div
+                className={`w-10 h-10 md:w-8 md:h-8 rounded-full flex-shrink-0 md:mr-4 border border-white flex items-center justify-center font-bold ${
+                  selectedIndex === index && "milky_cyan_bg text-black"
+                }`}
+              >
+                {index + 1}
               </div>
-            </Link>
+              <div className="uppercase hidden md:block">
+                <h4 className="text-xs opacity-60">Step {index + 1}</h4>
+                <h4 className="font-bold tracking-wider">{item.title}</h4>
+              </div>
+            </div>
           )
         })}
       </div>
--- src/pages/Step1.jsx
+++ src/pages/Step1.jsx
@@ -1,11 +1,64 @@
+import { useEffect, useState } from "react"
 import Sidebar from "../components/Sidebar"
 import { useFormData } from "../context/FormDataContext"
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 const Step1 = () => {
   const { useFormDataValues, changeName, changeEmail, changePhoneNumber } =
     useFormData()
   const { name, email, phoneNumber } = useFormDataValues()
+  const [nameError, setNameError] = useState("")
+  const [emailError, setEmailError] = useState("")
+  const [invalidEmail, setInvalidEmail] = useState("")
+  const [numberError, setNumberError] = useState("")
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+    if (email) {
+      setInvalidEmail(!emailRegex.test(email) ? true : false)
+    }
+  }, [email])
+
+  const handleNameChange = value => {
+    changeName(value)
+    if (value) {
+      if (nameError) setNameError("")
+    } else {
+      setNameError("This field is required")
+    }
+  }
+
+  const navigateTo = path => {
+    if (name && email && phoneNumber) {
+      navigate(path)
+    } else {
+      if (!name) setNameError("This field is required")
+      if (!email) setEmailError("This field is required")
+      if (!phoneNumber) setNumberError("This field is required")
+    }
+  }
+
+  const handleEmailChange = value => {
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+    changeEmail(value)
+    if (value) {
+      if (emailError) setEmailError("")
+      setInvalidEmail(!emailRegex.test(value) ? true : false)
+    } else {
+      setInvalidEmail("")
+      setEmailError("This field is required")
+    }
+  }
+
+  const handleNumberChange = value => {
+    changePhoneNumber(value)
+    if (value) {
+      if (numberError) setNumberError("")
+    } else {
+      setNumberError("This field is required")
+    }
+  }
 
   return (
     <>
@@ -21,45 +74,67 @@ const Step1 = () => {
                 Please provide your name, email address, and phone number.
               </p>
               <div className="mt-9">
-                <h5 className="pb-0.5 dark_blue_text font-[500] text-sm select-none">
-                  Name
-                </h5>
+                <div className="flex items-center mt-5 pb-0.5 font-[500] text-sm">
+                  <h5 className="dark_blue_text w-full">Name</h5>
+                  <p className="text-red-500 flex-shrink-0">{nameError}</p>
+                </div>
                 <input
                   id="step-1-name"
                   value={name}
-                  onChange={e => changeName(e.target.value)}
-                  className="mt-1.5 border pt-2.5 pb-3 border-neutral-300 font-[500] px-4 rounded-lg w-full"
+                  onChange={e => handleNameChange(e.target.value)}
+                  className={`mt-1.5 border pt-2.5 pb-3  font-[500] px-4 rounded-lg w-full ${
+                    nameError ? "border-red-500" : "border-neutral-300"
+                  }`}
                   placeholder="e.g. Stephen King"
+                  required
                 />
-                <h5 className="mt-5 pb-0.5 dark_blue_text font-[500] text-sm select-none">
-                  Email Address
-                </h5>
+                <div className="flex items-center mt-5 pb-0.5 font-[500] text-sm">
+                  <h5 className="dark_blue_text w-full">Email Address</h5>
+                  <p
+                    className={`${
+                      invalidEmail ? "text-yellow-500" : "text-red-500"
+                    } flex-shrink-0`}
+                  >
+                    {invalidEmail ? "Invalid email address" : emailError}
+                  </p>
+                </div>
                 <input
                   id="step-2-email"
                   value={email}
-                  onChange={e => changeEmail(e.target.value)}
-                  className="mt-1.5 border pt-2.5 pb-3 border-neutral-300 font-[500] px-4 rounded-lg w-full"
+                  pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
+                  onChange={e => handleEmailChange(e.target.value)}
+                  className={`mt-1.5 border pt-2.5 pb-3  font-[500] px-4 rounded-lg w-full ${
+                    emailError ? "border-red-500" : "border-neutral-300"
+                  }`}
                   placeholder="e.g. stephenking@lorem.com"
+                  required
                 />
-                <h5 className="mt-5 pb-0.5 dark_blue_text font-[500] text-sm select-none">
-                  Phone Number
-                </h5>
+                <div className="flex items-center mt-5 pb-0.5 font-[500] text-sm">
+                  <h5 className="dark_blue_text w-full">Phone Number</h5>
+                  <p className="text-red-500 flex-shrink-0">{numberError}</p>
+                </div>
                 <input
                   id="step-3-phone"
                   value={phoneNumber}
-                  onChange={e => changePhoneNumber(e.target.value)}
-                  className="mt-1.5 border pt-2.5 pb-3 border-neutral-300 font-[500] px-4 rounded-lg w-full"
+                  onChange={e => handleNumberChange(e.target.value)}
+                  className={`mt-1.5 border pt-2.5 pb-3  font-[500] px-4 rounded-lg w-full ${
+                    numberError ? "border-red-500" : "border-neutral-300"
+                  }`}
                   placeholder="e.g. +1 234 567 890"
+                  required
                 />
               </div>
             </div>
             <div className="flex-grow md:flex hidden items-end justify-end pt-6 pb-4">
-              <Link
-                to="/step-2"
+              <button
+                type="button"
+                onClick={() => {
+                  navigateTo("/step-2")
+                }}
                 className="rounded-lg dark_blue_bg text-white px-6 py-3 font-[500]"
               >
                 Next Step
-              </Link>
+              </button>
             </div>
           </div>
         </div>
@@ -67,11 +142,15 @@ const Step1 = () => {
 
       <div className="fixed w-full bottom-0 md:hidden">
         <div className="flex w-full bg-white bottom_nav_shadow p-6 flex-grow items-end justify-end">
-          <Link to="/step-2">
-            <button className="rounded-lg dark_blue_bg text-white px-6 py-3 font-[500]">
-              Next Step
-            </button>
-          </Link>
+          <button
+            type="button"
+            onClick={() => {
+              navigateTo("/step-2")
+            }}
+            className="rounded-lg dark_blue_bg text-white px-6 py-3 font-[500]"
+          >
+            Next Step
+          </button>
         </div>
       </div>
     </>
